efedf23a805f2fa232d183ff26558ec7c91432f1
Nguyen Quoc Bao <bao.nq@aris-vn.com>
2019-08-30 07:05:58 +0700

    log

--- DBI_Creator/Program.cs
+++ DBI_Creator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using DBI202_Creator.UI;
 
@@ -16,7 +18,23 @@ namespace DBI202_Creator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.Run(new CreatorForm());
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            string logPath = "./log/";
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+
+            File.AppendAllText(String.Concat(logPath, DateTime.Now.ToString("yyyy-MM-dd"), ".txt"), String.Concat(
+                "============\n",
+                "Message: ", e.Exception.Message, "\n",
+                "Trace: ", e.Exception.StackTrace, "\n"));
+            MessageBox.Show("Something not right, please send log file in log folder to me\nEmail: nguyenquocbaobm@gmail.com\nThank you!", "Error");
         }
     }
 }
--- DBI_Grading/Program.cs
+++ DBI_Grading/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 using DBI_Grading.UI;
 
@@ -14,7 +16,24 @@ namespace DBI_Grading
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.Run(new ImportForm());
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            string logPath = "./log/";
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+
+            File.AppendAllText(String.Concat(logPath, DateTime.Now.ToString("yyyy-MM-dd"), ".txt"), String.Concat(
+                "============\n",
+                "Message: ", e.Exception.Message, "\n",
+                "Trace: ", e.Exception.StackTrace, "\n"));
+            MessageBox.Show("Something not right, please send log file in log folder to me\nEmail: nguyenquocbaobm@gmail.com\nThank you!", "Error");
         }
     }
 }
